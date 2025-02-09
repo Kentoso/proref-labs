58daa700f9f3d4361fea6f1663ead4946279e953
Demian Samus <demiansamus2@gmail.com>
2025-02-09 10:58:25 +0200

    Added lexing without preprocessing

--- lab1/dbd.go
+++ lab1/dbd.go
@@ -4,67 +4,16 @@ import (
 	_ "embed"
 	"encoding/json"
 	"fmt"
-	"strings"
 )
-
-func removeLineNumber(line string) (string, error) {
-	if len(line) == 80 {
-		return line[:72], nil
-	}
-
-	return "", fmt.Errorf("invalid line length: %d", len(line))
-}
-
-func getContinuationLine(line string) string {
-	return line[14:]
-}
-
-func preprocessLines(input string) ([]string, error) {
-	lines := strings.Split(input, "\n")
-	var logicalLines []string
-	var cleanedLines []string
-
-	for i := 0; i < len(lines); i++ {
-		line, err := removeLineNumber(lines[i])
-		if err != nil {
-			return nil, err
-		}
-
-		cleanedLines = append(cleanedLines, line)
-	}
-
-	for i := 0; i < len(cleanedLines); i++ {
-		line := cleanedLines[i]
-		if line[len(line)-1] == '*' {
-			if i+1 < len(cleanedLines) {
-				nextLine := cleanedLines[i+1]
-				line = strings.TrimRight(line[:len(line)-1], " ") + getContinuationLine(nextLine)
-				i++
-			} else {
-				return nil, fmt.Errorf("%d: missing continuation line", i)
-			}
-		}
-
-		logicalLines = append(logicalLines, line)
-	}
-
-	return logicalLines, nil
-}
 
 //go:embed example.dbd
 var dbdInput string
 
 func main() {
-	logicalLines, err := preprocessLines(dbdInput)
+	tokens, err := lex(dbdInput)
 	if err != nil {
-		fmt.Println("Error preprocessing lines:", err)
+		fmt.Println("Error lexing:", err)
 		return
-	}
-
-	var tokens []Token
-
-	for _, line := range logicalLines {
-		tokens = append(tokens, lex(line)...)
 	}
 
 	parser := NewParser(tokens)
--- lab1/lexer.go
+++ lab1/lexer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"slices"
 	"unicode"
 )
@@ -8,25 +9,29 @@ import (
 type TokenType string
 
 const (
-	ILLEGAL TokenType = "ILLEGAL"
-	EOF     TokenType = "EOF"
-	EOL     TokenType = "EOL"
-	SEGM    TokenType = "SEGM"
-	FIELD   TokenType = "FIELD"
-	LCHILD  TokenType = "LCHILD"
-	XDFLD   TokenType = "XDFLD"
-	IDENT   TokenType = "IDENT"
-	ATTR    TokenType = "ATTR"
-	EQUALS  TokenType = "="
-	COMMA   TokenType = ","
-	LPAREN  TokenType = "("
-	RPAREN  TokenType = ")"
+	ILLEGAL    TokenType = "ILLEGAL"
+	EOF        TokenType = "EOF"
+	EOL        TokenType = "EOL"
+	SEGM       TokenType = "SEGM"
+	FIELD      TokenType = "FIELD"
+	LCHILD     TokenType = "LCHILD"
+	XDFLD      TokenType = "XDFLD"
+	IDENT      TokenType = "IDENT"
+	ATTR       TokenType = "ATTR"
+	EQUALS     TokenType = "="
+	COMMA      TokenType = ","
+	LPAREN     TokenType = "("
+	RPAREN     TokenType = ")"
+	SKIPLINE   TokenType = "SKIPLINE"
+	LINECONCAT TokenType = "LINECONCAT"
 )
 
 // Token represents a lexical token.
 type Token struct {
 	Type    TokenType
 	Literal string
+	Line    int
+	Column  int
 }
 
 var attributes = []string{
@@ -57,32 +62,83 @@ var attributes = []string{
 	"TYPE",
 }
 
-func lex(input string) []Token {
+type tokenCreator struct {
+	currentLine   *int
+	currentColumn *int
+}
+
+func (tc tokenCreator) createToken(tokenType TokenType, literal string) Token {
+	return Token{Type: tokenType, Literal: literal, Line: *tc.currentLine, Column: *tc.currentColumn}
+}
+
+func lex(input string) ([]Token, error) {
 	var tokens []Token
+	currentLine := 0
+	currentColumn := 0
+
+	tc := tokenCreator{currentLine: &currentLine, currentColumn: &currentColumn}
+
 	i := 0
 	for i < len(input) {
 		ch := input[i]
 
 		if ch == '\n' {
-			tokens = append(tokens, Token{Type: EOL, Literal: ""})
+			if currentColumn != 80 {
+				return nil, fmt.Errorf("line %d: line length is not equal to 80", currentLine)
+			}
+
+			tokens = append(tokens, tc.createToken(EOL, ""))
+			i++
+			currentLine++
+			currentColumn = 0
+			continue
 		}
 
 		if unicode.IsSpace(rune(ch)) {
+			currentColumn++
 			i++
 			continue
 		}
+
+		// skip line numbers
+		if currentColumn >= 72 {
+			i++
+			currentColumn++
+			continue
+		}
+
 		switch ch {
+		case '*':
+			if currentColumn != 71 {
+				return nil, fmt.Errorf("line %d column: %d: invalid line continuation position", currentLine, currentColumn)
+			}
+			
+			i += 9
+			currentColumn += 9
+			nextChar := input[i]
+
+			if nextChar == '\n' {
+				i++
+				currentLine++
+				currentColumn = 0
+			} else {
+				return nil, fmt.Errorf("line %d column: %d: no newline after line continuation", currentLine, currentColumn)
+			}
 		case '=':
-			tokens = append(tokens, Token{Type: EQUALS, Literal: "="})
+			tokens = append(tokens, tc.createToken(EQUALS, "="))
+			currentColumn++
 			i++
 		case ',':
-			tokens = append(tokens, Token{Type: COMMA, Literal: ","})
+			tokens = append(tokens, tc.createToken(COMMA, ","))
+			currentColumn++
 			i++
 		case '(':
-			tokens = append(tokens, Token{Type: LPAREN, Literal: "("})
+			tokens = append(tokens, tc.createToken(LPAREN, "("))
+			currentColumn++
 			i++
 		case ')':
-			tokens = append(tokens, Token{Type: RPAREN, Literal: ")"})
+			tokens = append(tokens, tc.createToken(RPAREN, ")"))
+			currentColumn++
 			i++
 		default:
 			start := i
@@ -90,22 +146,25 @@ func lex(input string) []Token {
 				input[i] != '=' && input[i] != ',' && input[i] != '(' && input[i] != ')' {
 				i++
 			}
+
 			tokenStr := input[start:i]
 			if tokenStr == "SEGM" {
-				tokens = append(tokens, Token{Type: SEGM, Literal: tokenStr})
+				tokens = append(tokens, tc.createToken(SEGM, tokenStr))
 			} else if tokenStr == "FIELD" {
-				tokens = append(tokens, Token{Type: FIELD, Literal: tokenStr})
+				tokens = append(tokens, tc.createToken(FIELD, tokenStr))
 			} else if tokenStr == "LCHILD" {
-				tokens = append(tokens, Token{Type: LCHILD, Literal: tokenStr})
+				tokens = append(tokens, tc.createToken(LCHILD, tokenStr))
 			} else if tokenStr == "XDFLD" {
-				tokens = append(tokens, Token{Type: XDFLD, Literal: tokenStr})
+				tokens = append(tokens, tc.createToken(XDFLD, tokenStr))
 			} else if slices.Contains(attributes, tokenStr) {
-				tokens = append(tokens, Token{Type: ATTR, Literal: tokenStr})
+				tokens = append(tokens, tc.createToken(ATTR, tokenStr))
 			} else {
-				tokens = append(tokens, Token{Type: IDENT, Literal: tokenStr})
+				tokens = append(tokens, tc.createToken(IDENT, tokenStr))
 			}
+
+			currentColumn += i - start
 		}
 	}
 	tokens = append(tokens, Token{Type: EOL, Literal: ""})
-	return tokens
+	return tokens, nil
 }
